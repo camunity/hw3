02354892c48f9cf93a2d83f2c3e824925d7fa58a
Cameron Flowers <cameron.flowers13@gmail.com>
2020-10-21 20:38:33 -0400

    edited App.js on dev

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is me changing the <code>src/App.js</code> on development branch.
         </p>
         <a
           className="App-link"
