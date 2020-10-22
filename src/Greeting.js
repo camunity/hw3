8c03f878382e4ce998f191f961cdc45325cd77fb
Cameron Flowers <cameron.flowers13@gmail.com>
2020-10-21 20:47:55 -0400

    adding Greeting component

---
+++ src/Greeting.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Greeting extends React.Component {
+
+    render(){
+        return(
+            <div>
+                Hello!
+            </div>
+        )
+    }
+
+
+
+}
